35cf96021bc2d1659da11df854cf509ba15068b6
okok7272 <kreuz1121@gmail.com>
2022-09-05 07:57:42 +0900

    my first deploy12

--- project/flask_app/__init__.py
+++ project/flask_app/__init__.py
@@ -15,28 +15,30 @@ dbpath = 'project\culture.db'
 @app.route('/',methods =['GET','POST'])
 def startpage():
     #해당 축제 선택'
-    conn = sqlite3.connect(dbpath)
+    conn = sqlite3.connect(r'C:\\Users\\InKoo\\Section3\\Cultural-Recommand\\project\\culture.db')
     cur = conn.cursor()
     culture = pd.read_sql("SELECT * FROM cultural;",conn, index_col=None)
 
     cultur2 =culture.copy()
-    cultur2.drop(columns=['end_period','start_period','door'])
+    cultur2.drop(columns=['end_period','start_period','door','region_number'])
     if request.method == 'POST':
         title = request.form['title_name']
         select_title=title
         select_cond = cultur2['title']==title
         select_data = cultur2[select_cond]
+        if(select_data.empty):
+            return redirect(url_for('/errorpage'))
         model=None
-        with open('project/model.pkl', 'rb') as pickle_file:
+        with open(r'C:\Users\InKoo\Section3\Cultural-Recommand\project\model.pkl', 'rb') as pickle_file:
             model=pickle.load(pickle_file)
         pred = model.predict(select_data)
-        return redirect(url_for(selectdata, door=pred))
+        return redirect(url_for('selectdata', door=pred))
 
-    return render_template('project\flask_app\main.html')
+    return render_template('main.html')
 
 @app.route('/select',methods =['GET','POST'])
 def selectdata(door):
-    conn = sqlite3.connect(dbpath)
+    conn = sqlite3.connect(r'C:\Users\InKoo\Section3\Cultural-Recommand\project\culture.db')
     cur = conn.cursor()
     culture = pd.read_sql("SELECT * FROM cultural;",conn, index_col=None)
 
@@ -45,7 +47,12 @@ def selectdata(door):
     cult3= culture[culture['region_number']==door1]
     cult3.to_json('project/cult.json',orient='records')
     cult4 = cult3
-    return render_template('project\flask_app\select.html',cult4)
+    return render_template('select.html',cult4)
+
+@app.route('/errorpage')
+def errorpage():
+
+    return render_template('errorpage.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
